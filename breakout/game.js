93f5c23c78226c8f82fc7b0d0c23f3c414763c1d
W Beecher Baker <wbbaker@purdue.edu>
2025-07-18 12:47:27 -0400

    refactor: Reposition score and lives display

--- breakout/game.js
+++ breakout/game.js
@@ -176,13 +176,18 @@ function drawScore() {
     ctx.font = '16px Arial';
     ctx.fillStyle = '#0095DD';
     ctx.fillText('Score: ' + score, 8, 20);
-    ctx.fillText('High Score: ' + highScore, 8, 40);
+
+    const highScoreText = 'High Score: ' + highScore;
+    const highScoreTextWidth = ctx.measureText(highScoreText).width;
+    ctx.fillText(highScoreText, (canvas.width - highScoreTextWidth) / 2, 20);
 }
 
 function drawLives() {
     ctx.font = '16px Arial';
     ctx.fillStyle = '#0095DD';
-    ctx.fillText('Lives: ' + lives, canvas.width - 65, 20);
+    const livesText = 'Lives: ' + lives;
+    const livesTextWidth = ctx.measureText(livesText).width;
+    ctx.fillText(livesText, canvas.width - livesTextWidth - 8, 20);
 }
 
 function draw() {
